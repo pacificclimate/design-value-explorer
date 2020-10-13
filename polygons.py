e883b3321432e764fb55c674930cbf3e2f3bbbff
Nic Annau <nicannau@gmail.com>
2020-10-13 16:23:13 -0700

    Fix default xarray and shape err

--- polygons.py
+++ polygons.py
@@ -14,25 +14,28 @@ def load_north_america_polygons_plotly(path_to_shapefile: str) -> str:
         instance is separated by a pair None, None. This is for plotly plotting purposes
         so that it doesn't connect islands to another land point
     """
-    cc = gpd.read_file(path_to_shapefile)
-    # include Canada, US, Greenland, Mexico, Cuba
-    cc = cc[
-        (cc.iso_a2 == "CA")
-        | (cc.iso_a2 == "US")
-        | (cc.iso_a2 == "GL")
-        | (cc.iso_a2 == "MX")
-        | (cc.iso_a2 == "CU")
-    ]
-    # exclude Hawaii
-    cc = cc[cc.name != "Hawaii"]
-    cc = cc.to_crs({"init": "epsg:4326"})
-    # extract geometric info of these areas
-    cc = cc.geometry
-    # rotate to rotated pole coordinates
-    canadar = rotate_shapefile(cc.geometry)
+    canada = gpd.read_file(path_to_shapefile).geometry
+    # # include Canada, US, Greenland, Mexico, Cuba
+    # cc = cc[
+    #     (cc.iso_a2 == "CA")
+    #     | (cc.iso_a2 == "US")
+    #     | (cc.iso_a2 == "GL")
+    #     | (cc.iso_a2 == "MX")
+    #     | (cc.iso_a2 == "CU")
+    # ]
+    # # exclude Hawaii
+    # cc = cc[cc.name != "Hawaii"]
+
+    # # cc = cc.to_crs({"init": "epsg:4326"})
+    # # extract geometric info of these areas
+    # # cc = cc.geometry
+    # # rotate to rotated pole coordinates
+
+    # canadar = rotate_shapefile(cc)
+    # print(canadar)
 
     pts = []
-    for poly in canadar:
+    for poly in canada:
         if isinstance(poly, MultiPolygon):
             for p in poly:
                 pts.extend(p.exterior.coords)
