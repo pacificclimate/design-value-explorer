a1d8596937e31de7c6f11f9f580d0b0a08efd511
rod-glover <rod.glover.ca@gmail.com>
2021-02-23 11:13:30 -0800

    Clean up run_app.py

--- run_app.py
+++ run_app.py
@@ -2,24 +2,12 @@ import yaml
 from dve.data import load_data
 from dve.app import get_app
 
-from frozendict import frozendict
-
-
-# TODO: Put this somewhere else
-def freeze(obj):
-    """Recursively freeze the objects in obj"""
-    if type(obj) == dict:
-        return frozendict((key, freeze(value)) for key, value in obj.items())
-    if type(obj) == list:
-        return tuple(freeze(value) for value in obj)
-    return obj
-
 
 print("\n\n### Running app")
 
 print(f"### Loading configuration")
 with open("config.yml", "r") as ymlfile:
-    config = freeze(yaml.load(ymlfile))
+    config = yaml.load(ymlfile)
 print(f"### Config loaded. {config}")
 
 print("### Calling load_data")
@@ -27,4 +15,11 @@ data = load_data(config)
 print("### Data loaded")
 
 app = get_app(config, data)
-# app.run_server(host='0.0.0.0', debug=True)
+
+if __name__ == "__main__":
+    print("### Running app")
+    app.run_server(
+        host='0.0.0.0',
+        port=5000,
+        debug=True,
+    )
