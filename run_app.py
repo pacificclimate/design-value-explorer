515c9efff482b4a679fbc2e5fdac4876f8c4657f
rod-glover <rod.glover.ca@gmail.com>
2021-03-03 17:08:40 -0800

    Add --debug argument to run_app.py

--- run_app.py
+++ run_app.py
@@ -1,7 +1,9 @@
+from argparse import ArgumentParser
 import yaml
 from dve.data import load_data
 from dve.app import get_app
 
+# TODO: Use logger instead of print statements
 
 print("\n\n### Running app")
 
@@ -17,9 +19,22 @@ print("### Data loaded")
 app = get_app(config, data)
 
 if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Run the Design Value Explorer Dash app"
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        default=False,
+        action="store_true",
+        help="Run in debug mode",
+    )
+    args = parser.parse_args()
+    print(f"args.debug={args.debug}")
+
     print("### Running app")
     app.run_server(
         host='0.0.0.0',
         port=5000,
-        debug=True,
+        debug=args.debug,
     )
