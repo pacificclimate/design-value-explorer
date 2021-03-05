dd19db9ae85391e1fb0268808e3e72dd0516b9a6
rod-glover <rod.glover.ca@gmail.com>
2021-03-05 14:53:56 -0800

    Remove entrypoint.sh

--- docker/production/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-
-#/app/.local/bin/gunicorn --config /app/docker/production/gunicorn.conf --log-config /app/docker/production/logging.conf wsgi:server
-#/app/.local/bin/gunicorn \
-#  --config /app/docker/production/gunicorn.conf \
-#  --log-config /app/docker/production/logging.conf \
-#  wsgi:server
-# /app/.local/bin/gunicorn -w 4 -b 0.0.0.0:5000 wsgi:server
-# /app/.local/bin/gunicorn -w 1 --log-level DEBUG --timeout 60 -b 0.0.0.0:5000 wsgi:server
-/bin/bash
