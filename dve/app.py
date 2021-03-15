a1e56cd7370fdfe859dd1efa6748e0bd08c0a142
rod-glover <rod.glover.ca@gmail.com>
2021-03-15 15:42:45 -0700

    Cleanup

--- dve/app.py
+++ dve/app.py
@@ -1,4 +1,3 @@
-import json
 import yaml
 
 import dve
@@ -10,39 +9,20 @@ import dve.data
 import dve.layout
 
 import dash
-from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
-import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_daq as daq
-import numpy as np
-
-from .data import get_data
-from .map_utils import (
-    pointer_rlonlat,
-    rlonlat_to_rindices,
-    pointer_rindices,
-    rindices_to_lonlat,
-    pointer_value,
-)
-from .download_utils import (
-    download_filename,
-    download_filepath,
-    download_base_url,
-    download_url,
-)
+from .download_utils import download_base_url
 
 import flask
 import os
 import warnings
 import logging
-import csv
 
 
 logger = logging.getLogger("dve")
 
 
 # TODO: This "app factory" needs to be refactored.
+
 
 def make_app(config_filepath="config.yml"):
     logger.debug("Loading configuration")
@@ -64,16 +44,16 @@ def get_app(config):
     app.title = "Pacific Climate Impacts Consortium Design Value Explorer"
     app.config.suppress_callback_exceptions = True
 
+    # Add layout
     app.layout = dve.layout.main(config)
 
+    # Add callbacks
     dve.app_.table_c2.add_callbacks(app, config)
-
     dve.app_.colour_scale.add_callbacks(app, config)
-
     dve.app_.map_pointer.add_callbacks(app, config)
-
     dve.app_.map_figure.add_callbacks(app, config)
 
+    # Add routes
     @app.server.route(f"{str(download_base_url())}/<filename>")
     def serve_static(filename):
         return flask.send_from_directory(
