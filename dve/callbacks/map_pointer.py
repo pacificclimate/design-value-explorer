592b769a8e1c77152b43414c4a83a23805dd297f
rod-glover <rod.glover.ca@gmail.com>
2021-03-16 14:17:42 -0700

    Clean up code

--- dve/callbacks/map_pointer.py
+++ dve/callbacks/map_pointer.py
@@ -2,163 +2,106 @@ from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
-from dve.data import (get_data, dv_value)
+from dve.data import get_data, dv_value
 from dve.download_utils import (
     download_filename,
-    download_filepath,
     download_url,
     create_download_file,
 )
 from dve.map_utils import (
     pointer_rlonlat,
-    rlonlat_to_rindices,
     pointer_rindices,
     rindices_to_lonlat,
     pointer_value,
 )
 
 
+# TODO: Place somewhere else (layout.components)?
+def map_pointer_table(
+    rlon,
+    rlat,
+    config,
+    climate_regime,
+    selected_dv=None,
+    selected_dataset_id=None,
+):
+    """
+    Return a table listing values of design values at a location specified
+    by rotated coordinates rlon, rlat
+
+    :param rlon:
+    :param rlat:
+    :return:
+    """
+    if climate_regime == "historical":
+        header_row = ["Model Value", "Reconstruction Value"]
+        dataset_ids = ("model", "reconstruction")
+    else:
+        header_row = list(config["future_change_factors"]["ids"])
+        dataset_ids = tuple(config["future_change_factors"]["ids"])
+
+    return dbc.Table(
+        [
+            html.Thead(
+                html.Tr([html.Th("DV")] + [html.Th(hdg) for hdg in header_row])
+            ),
+            html.Tbody(
+                [
+                    html.Tr(
+                        [html.Th(design_value_id, style={"width": "5em"})]
+                        + [
+                            html.Td(
+                                round(
+                                    float(
+                                        dv_value(
+                                            rlon,
+                                            rlat,
+                                            config,
+                                            design_value_id,
+                                            climate_regime,
+                                            historical_dataset_id=dataset_id,
+                                            future_dataset_id=dataset_id,
+                                        )
+                                    ),
+                                    3,
+                                ),
+                                style={
+                                    "color": "red"
+                                    if design_value_id == selected_dv
+                                    and dataset_id == selected_dataset_id
+                                    else "inherit"
+                                },
+                            )
+                            for dataset_id in dataset_ids
+                        ]
+                    )
+                    for design_value_id in config["dvs"].keys()
+                ]
+            ),
+        ],
+        bordered=True,
+        size="sm",
+    )
+
+
+def value_table(*items):
+    return dbc.Table(
+        [
+            html.Tbody(
+                [
+                    html.Tr(
+                        [html.Th(name, style={"width": "5em"}), html.Td(value)]
+                    )
+                    for name, value in items
+                ]
+            )
+        ],
+        bordered=True,
+        size="sm",
+    )
+
+
 def add(app, config):
-
-    # TODO: Extract these?
-    def value_table(*items):
-        return dbc.Table(
-            [
-                html.Tbody(
-                    [
-                        html.Tr(
-                            [
-                                html.Th(name, style={"width": "5em"}),
-                                html.Td(value),
-                            ]
-                        )
-                        for name, value in items
-                    ]
-                )
-            ],
-            bordered=True,
-            size="sm",
-        )
-
-    # TODO: Unify dv_table and cf_table
-    def dv_table(rlon, rlat, selected_dv=None, selected_dataset_id=None):
-        """
-        Return a table listing values of design values at a location specified
-        by rotated coordinates rlon, rlat
-
-        :param rlon:
-        :param rlat:
-        :return:
-        """
-        return dbc.Table(
-            [
-                html.Thead(
-                    [
-                        html.Tr(
-                            [
-                                html.Th("DV"),
-                                html.Th("Model"),
-                                html.Th("Reconstruction"),
-                            ]
-                        )
-                    ]
-                ),
-                html.Tbody(
-                    [
-                        html.Tr(
-                            [html.Th(design_value_id, style={"width": "5em"})]
-                            + [
-                                html.Td(
-                                    round(
-                                        float(
-                                            dv_value(
-                                                rlon,
-                                                rlat,
-                                                config,
-                                                design_value_id,
-                                                "historical",
-                                                historical_dataset_id=dataset_id,
-                                            )
-                                        ),
-                                        3,
-                                    ),
-                                    style={
-                                        "color": "red"
-                                        if design_value_id == selected_dv
-                                           and dataset_id == selected_dataset_id
-                                        else "inherit"
-                                    },
-                                )
-                                for dataset_id in ("model", "reconstruction")
-                            ]
-                        )
-                        for design_value_id in config["dvs"].keys()
-                    ]
-                ),
-            ],
-            bordered=True,
-            size="sm",
-        )
-
-    def cf_table(rlon, rlat, selected_dv=None, selected_dataset_id=None):
-        """
-        Return a table listing change factors for each design value at a
-        location specified by rotated coordinates rlon, rlat
-
-        :param rlon:
-        :param rlat:
-        :return:
-        """
-        return dbc.Table(
-            [
-                html.Thead(
-                    [
-                        html.Tr(
-                            [html.Th("DV")] + [
-                                html.Th(dataset_id)
-                                for dataset_id in config["future_change_factors"]["ids"]
-
-                            ]
-                        )
-                    ]
-                ),
-                html.Tbody(
-                    [
-                        html.Tr(
-                            [html.Th(design_value_id, style={"width": "5em"})]
-                            + [
-                                html.Td(
-                                    round(
-                                        float(
-                                            dv_value(
-                                                rlon,
-                                                rlat,
-                                                config,
-                                                design_value_id,
-                                                "future",
-                                                future_dataset_id=dataset_id,
-                                            )
-                                        ),
-                                        3,
-                                    ),
-                                    style={
-                                        "color": "red"
-                                        if design_value_id == selected_dv
-                                           and dataset_id == selected_dataset_id
-                                        else "inherit"
-                                    },
-                                )
-                                for dataset_id in config["future_change_factors"]["ids"]
-                            ]
-                        )
-                        for design_value_id in config["dvs"].keys()
-                    ]
-                ),
-            ],
-            bordered=True,
-            size="sm",
-        )
-
     @app.callback(
         Output("hover-info", "children"),
         [
@@ -176,13 +119,6 @@ def add(app, config):
         historical_dataset_id,
         future_dataset_id,
     ):
-        # TODO: Can we use a fixed value ("model" or "reconstruction") instead
-        #  of interpolation_ctrl? Note: Each type of dataset has a different
-        #  lat-lon grid.
-
-        # TODO: DRY this up with respect to display_click_info when we have
-        #   settled interface.
-
         if hover_data is None:
             return None
 
@@ -203,13 +139,7 @@ def add(app, config):
                 ("Lat", round(lat, 6)),
                 ("Lon", round(lon, 6)),
                 # (f"Z ({design_value_id_ctrl}) ({source})", round(z, 6)),
-            ),
-            # dv_table(
-            #     rlon,
-            #     rlat,
-            #     selected_dv=design_value_id_ctrl,
-            #     selected_interp=interpolation_ctrl,
-            # ),
+            )
         ]
 
     # TODO: This can be better done by setting the "href" and "download"
@@ -284,10 +214,6 @@ def add(app, config):
         two parts: Download button and data display. Unfortunately this is
         repetitive but no other solution is known.
         """
-        # TODO: Can we use a fixed value ("model" or "reconstruction" instead
-        #  of interp_ctrl? ... The grids for each are different and
-        #  give different values for lat/lon at the same pointer locn.
-        # TODO: DRY
         if click_data is None:
             return None
 
@@ -314,16 +240,12 @@ def add(app, config):
                 ("Lon", round(lon, 6)),
                 # (f"Z ({design_value_id_ctrl}) ({source})", round(z, 6)),
             ),
-            dv_table(
+            map_pointer_table(
                 rlon,
                 rlat,
+                config,
+                climate_regime,
                 selected_dv=design_value_id_ctrl,
                 selected_dataset_id=historical_dataset_id,
-            ) if climate_regime == "historical" else
-            cf_table(
-                rlon,
-                rlat,
-                selected_dv=design_value_id_ctrl,
-                selected_dataset_id=future_dataset_id,
             ),
         ]
