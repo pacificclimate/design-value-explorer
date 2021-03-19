dc75b0b85857e98f9d8479ff98d50838032b48f8
rod-glover <rod.glover.ca@gmail.com>
2021-03-19 10:06:48 -0700

    Add climate regime checking functions to config utils

--- dve/config.py
+++ dve/config.py
@@ -21,13 +21,29 @@ def validate(config):
     for dv_id in config["ui"]["dvs"]:
         dv_defn = config["dvs"][dv_id]
 
-        for key in (
-            "station_path",
-            "input_model_path",
-            "reconstruction_path",
-            "table",
-        ):
-            validate_filepath(dv_defn[key])
+        if dv_has_historical(config, dv_id):
+            for key in (
+                "station_path",
+                "input_model_path",
+                "reconstruction_path",
+                "table",
+            ):
+                validate_filepath(dv_defn[key])
 
         for fcf_id in config["ui"]["future_change_factors"]:
             validate_filepath(dv_defn["future_change_factor_paths"][fcf_id])
+
+
+# TODO: Deprecated
+def dv_has_historical(config, design_value_id):
+    return dv_has_climate_regime(config, design_value_id, "historical")
+
+
+def dv_has_climate_regime(config, design_value_id, climate_regime):
+    """
+    Return a boolean indicating whether a DV has definitions for specific
+    climate regime (historical or future) datasets.
+    """
+    return (
+        "abs_units" if climate_regime == "historical" else "cf_units"
+    ) in config["dvs"][design_value_id]
