4d578cebec0ac1850a2d1271e889b66afc1a7a85
rod-glover <rod.glover.ca@gmail.com>
2021-08-04 09:13:40 -0700

    WIP, working

--- docker/dev-local/entrypoint.sh
+++ docker/dev-local/entrypoint.sh
@@ -1,4 +1,5 @@
 # Install the package from local codebase
+pip install -e git+https://github.com/pacificclimate/climpyrical.git#egg=climpyrical
 pip install -e .
 #python dve_app.py --debug
 /bin/bash
