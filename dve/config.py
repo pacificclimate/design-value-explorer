64ae2ca79e62c3cd087e327296f8554e0456dcdd
rod-glover <rod.glover.ca@gmail.com>
2021-09-24 09:21:08 -0700

    Extend path_get to split string paths

--- dve/config.py
+++ dve/config.py
@@ -8,18 +8,19 @@ import os.path
 from pkg_resources import resource_filename
 
 
-def path_get(d, path, default=None):
+def path_get(d, path, default=None, separator="."):
     """
     Get a value addressed by `path` from a dict `d`.
     If `path` is not valid within the dict, return `default`.
     """
     if not isinstance(d, dict):
         return default
+    if isinstance(path, str):
+        return path_get(d, path.split(separator), default)
     if isinstance(path, (list, tuple)):
         if len(path) == 1:
             return d.get(path[0], default)
         return path_get(d[path[0]], path[1:], default)
-    return d.get(path, default)
 
 
 def validate_filepath(filepath):
