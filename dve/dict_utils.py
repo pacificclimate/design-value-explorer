7de296cdbc5262f56f1cd28cce3129a8ce44a40a
rod-glover <rod.glover.ca@gmail.com>
2021-09-24 14:36:20 -0700

    Add path_set to dict_utils

--- dve/dict_utils.py
+++ dve/dict_utils.py
@@ -1,6 +1,6 @@
 def path_get(d, path, default=None, separator="."):
     """
-    Get a value addressed by `path` from a dict `d`.
+    Get a value addressed by `path` from dict `d`.
     If `path` is not valid within the dict, return `default`.
     """
     if not isinstance(d, dict):
@@ -12,4 +12,22 @@ def path_get(d, path, default=None, separator="."):
             return d.get(path[0], default)
         return path_get(d[path[0]], path[1:], default)
 
+
+def path_set(d, path, value, separator="."):
+    """
+    Set a value addressed by `path` in dict `d`.
+    If an element of `path` does not exist within the dict, create sub-dicts
+    as necessary.
+    """
+    assert isinstance(d, dict)
+    if isinstance(path, str):
+        path_set(d, path.split(separator), value)
+    if isinstance(path, (list, tuple)):
+        key = path[0]
+        if len(path) == 1:
+            d[key] = value
+        else:
+            if key not in d or not isinstance(d[key], dict):
+                d[key] = {}
+            path_set(d[key], path[1:], value)
 
--- tests/test_dict_utils.py
+++ tests/test_dict_utils.py
@@ -1,13 +1,10 @@
+import copy
 import pytest
-from dve.dict_utils import path_get
+from dve.dict_utils import path_get, path_set
 
-d1 = {
-    "a": {
-        "b": {
-            "c": 42,
-        }
-    }
-}
+d0 = {}
+d1 = {"a": {"b": {"c": 42}}}
+
 
 @pytest.mark.parametrize(
     "d, path, default, expected",
@@ -30,3 +27,23 @@ d1 = {
 )
 def test_path_get(d, path, default, expected):
     assert path_get(d, path, default) == expected
+
+
+@pytest.mark.parametrize(
+    "d, path, value, expected",
+    [
+        (d0, "a", 99, {"a": 99}),
+        (d0, "a.b", 99, {"a": {"b": 99}}),
+        (d1, "a", 99, {"a": 99}),
+        (d1, "a.b", 99, {"a": {"b": 99}}),
+        (d1, "a.b.c", 99, {"a": {"b": {"c": 99}}}),
+        (d1, "x", 99,   {"a": {"b": {"c": 42}}, "x": 99}),
+        (d1, "a.x", 99, {"a": {"b": {"c": 42}, "x": 99}}),
+        (d1, "a.b.x", 99, {"a": {"b": {"c": 42, "x": 99}}}),
+        (d1, "a.b.c.x", 99, {"a": {"b": {"c": {"x": 99}}}}),
+    ]
+)
+def test_path_set(d, path, value, expected):
+    e = copy.deepcopy(d)
+    path_set(e, path, value)
+    assert e == expected
