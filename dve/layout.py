dcbe1826078b3f5df243a067e085631b67cc6941
rod-glover <rod.glover.ca@gmail.com>
2022-02-09 11:12:17 -0800

    Improve version display

--- dve/layout.py
+++ dve/layout.py
@@ -7,6 +7,7 @@ from dve.config import dv_label
 
 
 def interpret(source):
+    os.environ['DVE_VERSION_TAG_ONLY'] = os.environ['DVE_VERSION'].split()[0]
     return dcc.Markdown(
         source.format(**os.environ), dangerously_allow_html=True
     )
