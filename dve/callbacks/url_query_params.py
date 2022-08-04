bf096670b19d9850550dfb56a6104f2a7cb6d105
rod-glover <rod.glover.ca@gmail.com>
2022-08-03 19:35:51 -0700

    Replace bad qp value with default

--- dve/callbacks/url_query_params.py
+++ dve/callbacks/url_query_params.py
@@ -19,15 +19,16 @@ def add(app, config):
         ctx = dash.callback_context
         trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
 
+        qps_out = f"?dv={design_variable}"
+
         if trigger_id == "design_variable":
-            return dash.no_update, f"?dv={design_variable}"
+            return dash.no_update, qps_out
 
         qps = urllib.parse.parse_qs(url_search[1:])
-        # print("### query params", qps)
         if "dv" in qps:
             dv = qps["dv"][0]
             if dv in config["values"]["ui"]["dvs"]:
                 return dv, dash.no_update
-        return dash.no_update, dash.no_update
+        return dash.no_update, qps_out
 
 
